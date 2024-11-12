c5b4cfdcf108cac6b7a095827d1669415d1e2f2f
jumanapa07 <jumanajumi8822@gmail.com>
2024-11-13 00:07:56 +0530

    Fix errors and update logic,Finalise API implementation

--- Invoice_API/invoice/models.py
+++ Invoice_API/invoice/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Invoice(models.Model):
-    invoiceNum = models.CharField(max_length=30,unique=True)
+    invoice_number = models.CharField(max_length=30,unique=True)
     date=models.DateField()
-    customerName=models.CharField(max_length=100)
+    customer_name=models.CharField(max_length=100)
 
     def __str__(self):
         return self.invoiceNum
 
 class InvoiceDetail(models.Model):
-    invoice=models.ForeignKey(Invoice,on_delete=models.CASCADE)
+    invoice=models.ForeignKey(Invoice,related_name='details',on_delete=models.CASCADE)
     description=models.CharField(max_length=200)
     quantity=models.IntegerField()
     price=models.DecimalField(max_digits=10,decimal_places=2)
--- Invoice_API/invoice/serializers.py
+++ Invoice_API/invoice/serializers.py
@@ -5,34 +5,34 @@ class InvoiceDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model =InvoiceDetail
         fields=['id','description','quantity','price','line_total']
-        
+        read_only_fields = ['line_total']
 
 class InvoiceSerializer(serializers.ModelSerializer):
     details=InvoiceDetailSerializer(many=True)
 
     class Meta:
         model = Invoice
-        fields = ['id', 'invoiceNum', 'customerName', 'date', 'details']
+        fields = ['id', 'invoice_number', 'customer_name', 'date', 'details']
 
-        def create(self,validated_data):
-            details_data=validated_data.pop('details')
-            #invoice row created
-            invoice= Invoice.objects.create(**validated_data)
-            # each details of specific invoice are added to invoicedetail
-            for data in details_data:
-                InvoiceDetail.objects.create(invoice-invoice,**data)
+    def create(self,validated_data):
+        details_data=validated_data.pop('details')
+        #invoice row created
+        invoice= Invoice.objects.create(**validated_data)
+        # each details of specific invoice are added to invoicedetail
+        for data in details_data:
+            InvoiceDetail.objects.create(invoice=invoice,**data)
 
-            return invoice
-        def update( self , instance , validated_data):
-            details_data=validated_data.pop('details')
-            #update each invoice model fields
-            instance.invoiceNum=validated_data.get('invoiceNum',instance.invoiceNumber)
-            instance.customer_name = validated_data.get('customer_name', instance.customer_name)
-            instance.date = validated_data.get('date', instance.date)
-            instance.save()
-            # removed previous details and add updates ones
-            instance.details.all().delete()
-            for data in details_data:
-                InvoiceDetail.objects.create(invoice=instance,**data)
-            return instance
+        return invoice
+    def update( self , instance , validated_data):
+        details_data=validated_data.pop('details')
+        #update each invoice model fields
+        instance.invoice_number=validated_data.get('invoice_number',instance.invoice_number)
+        instance.customer_name = validated_data.get('customer_name', instance.customer_name)
+        instance.date = validated_data.get('date', instance.date)
+        instance.save()
+        # removed previous details and add updates ones
+        instance.details.all().delete()
+        for data in details_data:
+            InvoiceDetail.objects.create(invoice=instance,**data)
+        return instance
         
